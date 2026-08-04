55bafcd26dce696eaa02e06ce8691288e40c6856
Praveen <praveenpujari874@gmail.com>
2026-08-04 12:26:43 +0530

    actually second

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/P1.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/P1.java
@@ -1,6 +1,6 @@
 class P1 {
     public static void main (String[] args){
-       int num = 25;
+       int num = 0;
        if(num>0){
         System.out.println("Positive");
            }else if (num == 0){

45c9eac10486e042a95575fc8d1ba0d05321b880
Praveen <praveenpujari874@gmail.com>
2026-08-07 11:31:36 +0530

    initial

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/Userinput/Userinput.java
@@ -0,0 +1,37 @@
+package Userinput;
+
+import java.util.Scanner;
+
+public class Userinput {
+    public static void main(String[] args) {
+      java.util.Scanner scn = new Scanner(System.in);
+    System.out.print("Enter a byte Value: ");
+    byte a = scn.nextByte();
+    System.out.println("Byte value is:"+a);
+
+    System.out.print("Enter a short Value: ");
+    short b = scn.nextShort();
+    System.out.println("short value is:"+b);
+
+    System.out.print("Enter a int Value: ");
+    int c = scn.nextInt();
+    System.out.println("int value is:"+c);
+
+    System.out.print("Enter a long Value: ");
+    long d = scn.nextLong();
+    System.out.println("long value is:"+d);
+
+    System.out.print("Enter a float Value: ");
+    float e = scn.nextFloat();
+    System.out.println("float value is:"+e);
+
+    System.out.print("Enter a double Value: ");
+    double f = scn.nextDouble();
+    System.out.println("double value is:"+f);
+
+    System.out.print("Enter a boolean Value: ");
+    boolean g = scn.nextBoolean();
+    System.out.println("boolean value is:"+g);
+      scn.close();
+   }
+}
